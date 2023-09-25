af81e7631e90d859f5684bfe9348df56a31fe73f
Misbah Fatma <misbah7370@gmail.com>
2023-09-25 11:10:05 +0530

    first commit

---
+++ CLient/src/App.jsx
@@ -0,0 +1,25 @@
+
+import './App.css'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import CreateUser from './CreateUser'
+import UpdateUser from './UpdateUser'
+import User from './User'
+  
+function App() {
+ 
+
+  return (
+  <div>
+    <BrowserRouter>
+    <Routes>
+      <Route path = '/' element = {<User />}></Route>
+      <Route path = '/create' element = {<CreateUser/>}></Route>
+      <Route path = '/update/:id' element = {<UpdateUser />}></Route>
+      </Routes>
+      </BrowserRouter>
+  </div>
+  )
+}
+
+export default App
